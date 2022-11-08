73bd51555234a5378488c03e0cfcedfcf8ec3de2
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-11-08 20:14:38 +0600

    Add router to navbar & stylized

---
+++ src/component/Quotes.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './quotes.css';
+
+const Quotes = () => (
+  <div className="quotes">
+    <q>
+      Mathematic is not about numbers, equations, computations, or algorithms:
+      it is about understanding.
+    </q>
+    <h4>William Paul Thurston</h4>
+  </div>
+);
+
+export default Quotes;
