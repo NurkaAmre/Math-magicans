5eedaecf38120ba4568477fd62b6b667f20722ac
NurkaAmre <107000157+NurkaAmre@users.noreply.github.com>
2022-11-10 13:13:23 +0600

    unit testing react snapshot

--- src/_mock__/componentTest/quotes.test.js
+++ src/_mock__/componentTest/quotes.test.js
@@ -1,5 +1,5 @@
 import { render } from '@testing-library/react';
-import NavBar from '../../component/Quotes';
+
 
 describe('Quote', () => {
     test('should render quotes', () => {
--- src/_mock__/logicTest/calculate.test.js
+++ src/_mock__/logicTest/calculate.test.js
@@ -1,4 +1,3 @@
-import { render } from '@testing-library/react'
 import calculate from '../../logic/calculate'
 
 describe('calculate', () => {
